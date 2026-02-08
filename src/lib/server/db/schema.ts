d394917d92a4d067dc3cc63764355b8d981c074b
Elijah Styler <elijahstyler@gmail.com>
2026-02-07 19:56:15 -0700

    add in database schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,19 @@
+import { sql } from "drizzle-orm"
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const task = sqliteTable('task', {
+export const items = sqliteTable('items', {
 	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+	date: text('date').default(sql`CURRENT_DATE`),
+	desc: text('desc').notNull(),
+	img: text('img').notNull(), // base64 encoded
+	claimed: integer('claimed', { mode: "boolean" }).notNull(),
+});
+
+export const claims = sqliteTable('claims', {
+	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+	item: text('item').notNull(), // id of item
+	name: text('name').notNull(), // in case of stalking
+	tel: integer('tel'),
+	email: text('email'),
+	status: integer('status').notNull(),
 });
