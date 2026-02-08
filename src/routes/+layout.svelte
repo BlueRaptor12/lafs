2f56d08989f4372b9f166acb7d49f43353434c68
Elijah Styler <elijahstyler@gmail.com>
2026-02-07 20:26:01 -0700

    implement navbar and style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 
+	import Navbar from '$lib/navbar.svelte';
+
+	import style from '$lib/style.css';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href={style} />
 </svelte:head>
 
+
+<Navbar />
 {@render children()}
